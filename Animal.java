c072143f6b45f7afb5e7226d1fe50798262b32c1
James Bury <jamesbury77@gmail.com>
2019-11-22 12:00:22 -0500

    Adding Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+
+    private String name;
+    private String furLength;
+
+    public Animal(String name, int furLength) {
+        this.name = name;
+        this.furLength = furLength;
+    }
+}
